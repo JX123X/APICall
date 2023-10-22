3332c162b7559c2185ba7e7645b9763edcc4c7f3
JX <99164789+JX123X@users.noreply.github.com>
2023-10-22 15:02:28 -0700

    first commit

---
+++ StockApp/StockApp/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  StockApp
+//
+//  Created by X on 10/21/23.
+//
+
+import Foundation
+
+let apiKey = "0486072ee4bf6c33715950eccb051360"
+let baseURL = "https://financialmodelingprep.com/api/v3/"
